e3cfaa2548810a0cee6179462f056a795ac0c353
gadgallea <gadgallea@gmail.com>
2022-03-05 13:03:44 +0200

    t

---
+++ src/services/Microservice1/Microservice.Core.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microservice.Core.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
